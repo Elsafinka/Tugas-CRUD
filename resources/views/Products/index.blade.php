96f5ab60968b82af7fe43357e155879dd6e0b924
elsafinka <elsa.alia.safinka@gmail.com>
2024-10-22 22:02:25 +0700

    first init github

---
+++ resources/views/Products/index.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Tugas CRUD </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+</head>
+<body style="background: lightgray">
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 shadow rounded">
+                    <h1 text-align: center>Tugas CRUD Elsa</h1>
+                    <div class="card-body">
+                        <a href="{{ route('products.create') }}" class="btn btn-md btn-success mb-3">TAMBAH BLOG</a>
+                        <table class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <th scope="col">IMAGE</th>
+                                <th scope="col">TITLE</th>
+                                <th scope="col">PRICE</th>
+                                <th scope="col">STOCK</th>
+                                <th scope="col" style="width: 20%">ACTIONS</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @forelse ($products as $blog)
+                                <tr>
+                                    <td class="text-center">
+                                        <img src="{{ asset('storage/products/'.$blog->image) }}" class="rounded" style="width: 150px">
+                                    </td>
+                                    <td>{{ $blog->title }}</td>
+                                    <td>{!! $blog->description !!}</td>
+                                    <td>{{ $blog->stock }}</td>
+                                    <td class="text-center">
+                                        <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('products.destroy', $blog->id) }}" method="POST">
+                                            <a href="{{ route('products.edit', $blog->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @empty
+                                <div class="alert alert-danger">
+                                    Data Blog belum Tersedia.
+                                </div>
+                            @endforelse
+                            </tbody>
+                        </table>  
+                        {{ $products->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script>
+        //message with toastr
+        @if(session()->has('success'))
+        
+            toastr.success('{{ session('success') }}', 'BERHASIL!'); 
+
+        @elseif(session()->has('error'))
+
+            toastr.error('{{ session('error') }}', 'GAGAL!'); 
+            
+        @endif
+    </script>
+
+</body>
+</html>
